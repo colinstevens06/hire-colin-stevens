198cc5ed9f6b98e2a770de41f997ac50d2a071c1
Colin Stevens <colinstevens06@gmail.com>
2021-03-22 23:24:17 -0400

    modal-form setup on contact slice

--- client/src/components/slices/ContactMe.js
+++ client/src/components/slices/ContactMe.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { useState } from 'react'
 import { Link } from 'react-scroll'
 
+import { Modal, Button, Row, Col } from 'react-bootstrap'
+
 export default function ContactMe() {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClose = () => setShowModal(false);
+  const handleShow = () => setShowModal(true);
+
+
   return (
     <div className="image__background hero-image__contact-me" name="contact">
       <div className="container-text__contact-me">
@@ -17,9 +26,52 @@ export default function ContactMe() {
             <li>Music</li>
 
           </ul>
-          <p>Fill out this form or email me at colinstevens06@gmail.com.</p>
+          <p>You can email me at <a href="mailto:colinstevens06@gmail.com" className="email">colinstevens06@gmail.com</a> or fill out <span onClick={handleShow}>this form</span>.</p>
         </div>
       </div>
+      <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Contact Me</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form className="form__contact">
+            <Row>
+              <Col>
+                <input name="first-name" placeholder="First Name" />
+
+              </Col>
+              <Col>
+                <input name="last-name" placeholder="Last Name" />
+
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <input name="email" placeholder="Email" />
+
+              </Col>
+              <Col>
+                <input name="phone" placeholder="Phone" />
+
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <textarea name="message" placeholder="Tell me about your project" />
+              </Col>
+            </Row>
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Submit
+          </Button>
+        </Modal.Footer>
+
+      </Modal>
       <Link to="hero" spy={true} smooth={true} duration={500} className="text__back-to-top">Back to Top</Link>
     </div>
   )
--- client/src/components/slices/ThisIsMe.js
+++ client/src/components/slices/ThisIsMe.js
@@ -7,15 +7,13 @@ import { Link } from 'react-scroll'
 export default function ThisIsMe() {
   return (
     <div className="image__background no-overlay hero-image__this-is-me" name="this-is-me">
-      <div className="hero-image-text-overlay">
-        <div className="container__this-is-me-text">
-          <h2 className="header__this-is-me text-black">This is Me!</h2>
-          <p>I’m a creative person with a passion for life.</p>
+      <div className="container__this-is-me-text">
+        <h2 className="header__this-is-me text-black">This is Me!</h2>
+        <p>I’m a creative person with a passion for life.</p>
 
-          <p>I love building interactive JS applications, learning new ways to improve my code, and finding solutions to complex problems.</p>
+        <p>I love building interactive JS applications, learning new ways to improve my code, and finding solutions to complex problems.</p>
 
-          <p>In my spare time, you’ll find me outside, playing music or taking photos (including the photos on this site).</p>
-        </div>
+        <p>In my spare time, you’ll find me outside, playing music or taking photos (including the photos on this site).</p>
       </div>
       <div className="container__right-arrows">
         <Link to="hero" spy={true} smooth={true} duration={750}>
