810631bc2436748b91fa540d9e6287a2eddbf16f
Colin Stevens <colinstevens06@gmail.com>
2021-03-29 18:17:04 -0400

    mobile updates

--- client/src/components/my-projects/ProjectsCarousel.js
+++ client/src/components/my-projects/ProjectsCarousel.js
@@ -12,51 +12,49 @@ import { Link } from 'react-router-dom'
 
 export default function ProjectsCarousel() {
   return (
-    <div>
-      <Carousel interval={null}>
-        <Carousel.Item interval={12000}>
-          <div className="header__projects-slider-item">Life Organizer</div>
-          <div className="container__slider-item">
-            <div className="text__project-slider-item">
-              <p>This MERN project uses authentication, protected routes and a MongoDB database to store information.</p>
-              <p>I used Firebase to handle the authentication, then I use that authentication to serve up content specific to the user logged in.</p>
-              <p>Also found in this project: SASS, form handling, error handling</p>
-              <p><Link to="https://life-organizer-by-colin.herokuapp.com/" target="_blank">Visit the site</Link> or <Link to="https://github.com/colinstevens06/Life-Organizer" target="_blank">view the repo</Link>.</p>
-            </div>
-            <div className="image__project-slider-item">
-              <img src={img1} alt="Lies From Granny" />
-            </div>
+    <Carousel interval={null}>
+      <Carousel.Item interval={12000}>
+        <div className="header__projects-slider-item">Life Organizer</div>
+        <div className="container__slider-item">
+          <div className="text__project-slider-item">
+            <p>This MERN project uses authentication, protected routes and a MongoDB database to store information.</p>
+            <p>I used Firebase to handle the authentication, then I use that authentication to serve up content specific to the user logged in.</p>
+            <p>Also found in this project: SASS, form handling, error handling</p>
+            <p><Link to="https://life-organizer-by-colin.herokuapp.com/" target="_blank">Visit the site</Link> or <Link to="https://github.com/colinstevens06/Life-Organizer" target="_blank">view the repo</Link>.</p>
           </div>
-        </Carousel.Item>
-        <Carousel.Item interval={12000}>
-          <div className="header__projects-slider-item">Baltimore Happy Hours</div>
-          <div className="container__slider-item">
-            <div className="text__project-slider-item">
-              <p>This MERN project uses a lot of data and has state changes based on user interaction. </p>
-              <p>I created the API for this and use that API to populate templated pages.</p>
-              <p>I also placed an emphasis on creating a smooth user experience on mobile.</p>
-              <Link to="https://www.baltimorehappyhour.com/" target="_blank">Visit the site</Link>
-            </div>
-            <div className="image__project-slider-item">
-              <img src={img2} alt="Lies From Granny" />
-            </div>
+          <div className="image__project-slider-item">
+            <img src={img1} alt="Lies From Granny" />
           </div>
-        </Carousel.Item>
-        <Carousel.Item interval={12000}>
-          <div className="header__projects-slider-item">Lies From Granny</div>
-          <div className="container__slider-item">
-            <div className="text__project-slider-item">
-              <p>My band needed a website, so I designed this using React.</p>
-              <p>I wanted to create a simple design with photos being the emphasis of the design.</p>
-              <p>I used React for the front-end and SASS for styling.</p>
-              <p><Link to="http://liesfromgranny.com/" target="_blank">Visit the site</Link> or <Link to="https://github.com/colinstevens06/Lies-From-Granny" target="_blank">view the repo</Link>.</p>
-            </div>
-            <div className="image__project-slider-item">
-              <img src={img3} alt="Lies From Granny" />
-            </div>
+        </div>
+      </Carousel.Item>
+      <Carousel.Item interval={12000}>
+        <div className="header__projects-slider-item">Baltimore Happy Hours</div>
+        <div className="container__slider-item">
+          <div className="text__project-slider-item">
+            <p>This MERN project uses a lot of data and has state changes based on user interaction. </p>
+            <p>I created the API for this and use that API to populate templated pages.</p>
+            <p>I also placed an emphasis on creating a smooth user experience on mobile.</p>
+            <Link to="https://www.baltimorehappyhour.com/" target="_blank">Visit the site</Link>
           </div>
-        </Carousel.Item>
-      </Carousel>
-    </div>
+          <div className="image__project-slider-item">
+            <img src={img2} alt="Lies From Granny" />
+          </div>
+        </div>
+      </Carousel.Item>
+      <Carousel.Item interval={12000}>
+        <div className="header__projects-slider-item">Lies From Granny</div>
+        <div className="container__slider-item">
+          <div className="text__project-slider-item">
+            <p>My band needed a website, so I designed this using React.</p>
+            <p>I wanted to create a simple design with photos being the emphasis of the design.</p>
+            <p>I used React for the front-end and SASS for styling.</p>
+            <p><Link to="http://liesfromgranny.com/" target="_blank">Visit the site</Link> or <Link to="https://github.com/colinstevens06/Lies-From-Granny" target="_blank">view the repo</Link>.</p>
+          </div>
+          <div className="image__project-slider-item">
+            <img src={img3} alt="Lies From Granny" />
+          </div>
+        </div>
+      </Carousel.Item>
+    </Carousel>
   )
 }
--- client/src/components/slices/Hero.js
+++ client/src/components/slices/Hero.js
@@ -10,7 +10,7 @@ export default function Hero() {
     <div className="image__background hero-image" name="hero">
       <div className="hero-image-text-overlay">
         <div className="hero-image-text-overlay__header">Colin Stevens</div>
-        <div className="hero-image-text-overlay__subheader">&nbsp;Web Developer</div>
+        <div className="hero-image-text-overlay__subheader">&nbsp;Web Developer<br />&nbsp;Photographer<br />&nbsp;Musician<br />&nbsp;Rad Dude</div>
       </div>
       <Link to="this-is-me" spy={true} smooth={true} duration={750}>
         <FontAwesomeIcon icon={faLongArrowAltDown} className="down-arrow" />
